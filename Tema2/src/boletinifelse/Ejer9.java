447e368e7baa440a78e2fdaa5756c588b00d3006
ruben.carrasco <ruben.carrasco@106-05.info2>
2024-10-07 13:17:51 +0200

    Ejer 9 y 10

---
+++ Tema2/src/boletinifelse/Ejer9.java
@@ -0,0 +1,20 @@
+package boletinifelse;
+
+import java.util.Scanner;
+
+public class Ejer9 {
+	public static void main(String[] args) {
+		// Declaramos las variables para recojer el resultado de cada jugador
+		String j1;
+		String j2;
+		
+		// Creamos el Scanner
+		Scanner reader = new Scanner(System.in);
+		
+		// 
+		
+		// Cerramos el Scanner
+		reader.close();
+	}
+
+}
