dd82eca4519592d81672c7574768ab9b54b4bce5
ruben.carrasco <ruben.carrasco@106-05.info2>
2024-10-08 14:39:11 +0200

    ejer 9

--- Tema2/src/boletinifelse/Ejer9.java
+++ Tema2/src/boletinifelse/Ejer9.java
@@ -7,11 +7,50 @@ public class Ejer9 {
 		// Declaramos las variables para recojer el resultado de cada jugador
 		String j1;
 		String j2;
+		// Declaramos las constantes
+		final String PIEDRA = "PIEDRA";
+		final String PAPEL = "PAPEL";
+		final String TIJERAS = "TIJERAS";
 		
 		// Creamos el Scanner
 		Scanner reader = new Scanner(System.in);
 		
-		// 
+		// Pedimos y leemos el resultado del jugador 1
+		System.out.println("Jugador 1, elija PIEDRA, PAPEL o TIJERAS: ");
+		j1 = reader.nextLine();
+		
+		// Pedimos y leemos el resultado del jugador 2
+		System.out.println("Jugador 2, elija PIEDRA, PAPEL o TIJERAS: ");
+		j2 = reader.nextLine();
+				
+		// Creamos los condicionales
+		if ( j1.equals(j2)) {
+			System.out.println("Hay empate");
+		}
+		else if (j1.equals(PIEDRA) && j2.equals(PAPEL)) {
+			System.out.println("Gana jugador 2");
+			
+		}
+		else if (j1.equals(PIEDRA) && j2.equals(TIJERAS)) {
+			System.out.println("Gana jugador 1");
+			
+		}
+		else if (j1.equals(PAPEL) && j2.equals(TIJERAS)) {
+			System.out.println("Gana jugador 2");
+			
+		}
+		else if (j1.equals(PAPEL) && j2.equals(PIEDRA)) {
+			System.out.println("Gana jugador 1");
+			
+		}
+		else if (j1.equals(TIJERAS) && j2.equals(PIEDRA)) {
+			System.out.println("Gana jugador 2");
+			
+		}
+		else {
+			System.out.println("Gana jugador 1");
+		}
+		
 		
 		// Cerramos el Scanner
 		reader.close();
